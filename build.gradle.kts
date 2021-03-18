4a7c9ab7ca73bf24d6c1facdeb49c82e1e8ac0c9
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2021-03-18 16:46:55 +0100

    Added missing postgres driver as dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ val kafka_version: String by project
 val influxdb_version: String by project
 val klaxon_version: String by project
 val ojdbc_version: String by project
+val postgres_version: String by project
 val hikari_version: String by project
 val flyway_version: String by project
 
@@ -31,6 +32,7 @@ dependencies {
     implementation("org.influxdb:influxdb-java:$influxdb_version")
     implementation("com.beust:klaxon:$klaxon_version")
     implementation("com.oracle.database.jdbc:ojdbc8:$ojdbc_version")
+    implementation("org.postgresql:postgresql:$postgres_version")
     implementation("com.zaxxer:HikariCP:$hikari_version")
     implementation("org.flywaydb:flyway-core:$flyway_version")
 }
