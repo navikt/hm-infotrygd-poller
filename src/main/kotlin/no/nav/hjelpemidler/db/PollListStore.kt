b9fe6a219efc1d584ad9fd76f6420560f32020c0
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-05-02 13:17:47 +0200

    Removed tknr check again post db patch

--- src/main/kotlin/no/nav/hjelpemidler/db/PollListStore.kt
+++ src/main/kotlin/no/nav/hjelpemidler/db/PollListStore.kt
@@ -112,8 +112,6 @@ internal class PollListStorePostgres(private val ds: DataSource) : PollListStore
     }
 
     override fun getPollingBatch(size: Int): List<Poll> {
-        // FIXME: condition below "AND LENGTH(TKNR) = 4" is so that we stop spamming errors while I investigate why our
-        //  legacy code integration started intermittently misbehaving a few days ago
         @Language("PostgreSQL") val statement = """
             SELECT
                 SOKNADS_ID,
@@ -129,7 +127,7 @@ internal class PollListStorePostgres(private val ds: DataSource) : PollListStore
                 (LAST_POLL IS NULL AND CREATED + '10 minutes'::interval < NOW())
                 OR
                 LAST_POLL <= NOW() - '60 minutes'::interval
-            ) AND LENGTH(TKNR) = 4
+            )
             ORDER BY LAST_POLL ASC NULLS FIRST
             LIMIT $size
         """.trimIndent().split("\n").joinToString(" ")
