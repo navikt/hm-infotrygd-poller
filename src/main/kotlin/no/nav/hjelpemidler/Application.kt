9bc98961738bb7224c07c628b3502d0724763b3a
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2026-02-05 16:42:17 +0100

    Logg feilmeldinger i test api

--- src/main/kotlin/no/nav/hjelpemidler/Application.kt
+++ src/main/kotlin/no/nav/hjelpemidler/Application.kt
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.request.receive
+import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
@@ -101,49 +103,55 @@ fun main() {
                     call.respondText("OK")
                 }
                 post("/internal/brevstatistikk2") {
-                    data class Request(
-                        val enhet: String,
-                        val minVedtaksdato: LocalDate,
-                        val maksVedtaksdato: LocalDate,
-                        val digitaleOppgaveIder: Set<String>,
-                    )
-                    val req = call.receive<Request>()
-
-                    // Oppdater brevstatistikk
-                    logg.info { "Oppdaterer brevstatistikk2 (manuelt): enhet=${req.enhet}, minVedtaksdato=${req.minVedtaksdato}, maksVedtaksdato=${req.maksVedtaksdato}" }
-                    val brevstatistikk = Infotrygd().hentBrevstatistikk2(
-                        req.enhet,
-                        req.minVedtaksdato,
-                        req.maksVedtaksdato,
-                        req.digitaleOppgaveIder,
-                    )
-
-                    val eldste = brevstatistikk.fold(LocalDate.EPOCH) { eldste, row ->
-                        if (eldste == LocalDate.EPOCH) return@fold row.dato
-                        if (row.dato.isBefore(eldste)) {
-                            row.dato
-                        } else {
-                            eldste
-                        }
-                    }
-                    logg.info { "Fant ${brevstatistikk.count()} rader med brevstatistikk (eldste=$eldste)" }
-
-                    brevstatistikkStore.slettPeriode2(req.enhet, req.minVedtaksdato, req.maksVedtaksdato)
-                    brevstatistikk.forEach { row ->
-                        brevstatistikkStore.lagre2(
-                            row.enhet,
-                            row.dato,
-                            row.digital,
-                            row.brevkode,
-                            row.valg,
-                            row.undervalg,
-                            row.type,
-                            row.resultat,
-                            row.antall,
+                    runCatching {
+                        data class Request(
+                            val enhet: String,
+                            val minVedtaksdato: LocalDate,
+                            val maksVedtaksdato: LocalDate,
+                            val digitaleOppgaveIder: Set<String>,
                         )
-                    }
 
-                    call.respondText("OK")
+                        val req = call.receive<Request>()
+
+                        // Oppdater brevstatistikk
+                        logg.info { "Oppdaterer brevstatistikk2 (manuelt): enhet=${req.enhet}, minVedtaksdato=${req.minVedtaksdato}, maksVedtaksdato=${req.maksVedtaksdato}" }
+                        val brevstatistikk = Infotrygd().hentBrevstatistikk2(
+                            req.enhet,
+                            req.minVedtaksdato,
+                            req.maksVedtaksdato,
+                            req.digitaleOppgaveIder,
+                        )
+
+                        val eldste = brevstatistikk.fold(LocalDate.EPOCH) { eldste, row ->
+                            if (eldste == LocalDate.EPOCH) return@fold row.dato
+                            if (row.dato.isBefore(eldste)) {
+                                row.dato
+                            } else {
+                                eldste
+                            }
+                        }
+                        logg.info { "Fant ${brevstatistikk.count()} rader med brevstatistikk (eldste=$eldste)" }
+
+                        brevstatistikkStore.slettPeriode2(req.enhet, req.minVedtaksdato, req.maksVedtaksdato)
+                        brevstatistikk.forEach { row ->
+                            brevstatistikkStore.lagre2(
+                                row.enhet,
+                                row.dato,
+                                row.digital,
+                                row.brevkode,
+                                row.valg,
+                                row.undervalg,
+                                row.type,
+                                row.resultat,
+                                row.antall,
+                            )
+                        }
+
+                        call.respondText("OK")
+                    }.getOrElse { e ->
+                        logg.error(e) { "Feilet i å hente brevstatistikk2" }
+                        call.respond(HttpStatusCode.InternalServerError, "Feilet i å hente brevstatistikk2")
+                    }
                 }
             }
         }.build().apply {
